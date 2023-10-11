1dc67c49f2e26cf02ecf27f0fbd79752a137c982
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-10-10 23:57:08 -0300

    adicionei o módulo que baixa a base de dados, a base de dados e os metadados
    agora há um módulo para baixar os dados da base de dados e salvá-los como csv ou apenas deixar como um data frame pandas, a base de dados dividida em anos se encontra no diretório dados, onde é da forma Manipulados_{ano dos dados}.csv e os meta dados se encontram como um pdf no diretório metadados em dados. Ainda falta melhorar boa parte dos códigos do módulo get_data.

---
+++ functions/get_data.py
@@ -0,0 +1,57 @@
+"""Módulo que coleta os dados de Venda de Medicamentos Controlados e Antimicrobianos (Medicamentos Manipulados)
+do site "dados.gov.br", dados disponíveis de Janeiro de 2014 até Novembro de 2021.
+Para a plena execução deste módulo, é necessário ter certeza de que o domínio
+https://dados.gov.br/dados/conjuntos-dados/venda-de-medicamentos-controlados-e-antimicrobianos---medicamentos-manipulados
+se encontra em pleno funcionamente e não está fora do ar, além disso, não houve alteração na forma como os dados são armazenados.
+"""
+
+import pandas as pd
+
+
+def validate_dates():
+    pass
+
+def get_dates_between_dates(first_date: str, final_date: str):
+    dates_list = []
+    first_year = int(first_date[:4])
+    first_month = int(first_date[-2:])
+    final_year = int(final_date[:4])
+    final_month = int(final_date[-2:])
+
+    while first_year <= final_year:
+        date = f"{first_year}{first_month:02}"
+        dates_list.append(date)
+        
+        if first_year == final_year and first_month == final_month:
+            break
+        elif first_month == 12:
+            first_year += 1
+            first_month = 1
+        else:
+            first_month += 1
+
+    return dates_list
+
+
+def data_to_csv_by_dates(start_date: str, end_date: str, output_file = None):
+    dataset = pd.read_csv(f"https://dados.anvisa.gov.br/dados/SNGPC/Manipulados/EDA_Manipulados_{start_date}.csv", delimiter=";", encoding="unicode_escape", low_memory=False)
+
+    if start_date != end_date:
+        dates = get_dates_between_dates(start_date, end_date)
+
+        for i in range(1, len(dates)):
+            new_year_data = pd.read_csv(f"https://dados.anvisa.gov.br/dados/SNGPC/Manipulados/EDA_Manipulados_{dates[i]}.csv", delimiter=";", encoding="unicode_escape", low_memory=False)
+            dataset = pd.concat([dataset, new_year_data])
+
+    dataset.to_csv(output_file, sep=";", index=False)
+
+    return dataset
+
+
+if __name__ == "__main__":
+    # Baixando os dados para que eles fiquem salvos para futuras manipulações
+    """
+    for ano in range(2014, 2021):
+        data_to_csv_by_dates(f"{ano}01", f"{ano}12", output_file=f"dados/Manipulados_{ano}.csv")
+    data_to_csv_by_dates(f"202101", f"202111", output_file=f"dados/Manipulados_2021.csv")
+    """
