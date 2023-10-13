dde55493a7b51313e08dfeb945a149b803c530df
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-10-13 19:42:59 -0300

    adicionei a função que cria a visualização da cloroquina
    a visualização é um heatmap sobre os estados com maior número de vendas em cloroquina, será necessário para a criação da visualização final

--- functions/sillas_vis.py
+++ functions/sillas_vis.py
@@ -2,12 +2,8 @@ import geobr
 import pandas as pd
 import matplotlib.pyplot as plt
 import utils
+from get_data import get_dates_between_dates
 
-
-def filtra_dados_por_valores_procurados(dados: pd.DataFrame, coluna_do_valor: str, valores_procurados: list) -> pd.DataFrame:
-    dados = dados[dados[coluna_do_valor] in valores_procurados]
-
-    return dados
 
 def soma_vendas_por_atributo(dados: pd.DataFrame, atributo: str) -> pd.DataFrame:
     soma_vendas = pd.DataFrame()
@@ -28,19 +24,38 @@ def mapeia_dados_estaduais(dados_mapeamento: pd.DataFrame, coluna_estados: str,)
     return dados_resultantes
 
 
+def gera_visualizacao_cloroquina(dados: pd.DataFrame, coluna_principio_ativo="PRINCIPIO_ATIVO",
+                                 coluna_estados="UF_VENDA", cmap="plasma", vmax=3052, show_figure=False) -> pd.DataFrame:
+    principios_ativos_cloroquina = [
+        "CLOROQUINA",
+        "DIFOSFATO DE CLOROQUINA",
+        "HIDROXICLOROQUINA",
+        "SULFATO DE HIDROXICLOROQUINA",
+        "DICLORIDRATO DE CLOROQUINA",
+        "SULFATO DE CLOROQUINA",
+    ]
+    fig, ax = plt.subplots(figsize=(15, 15))
+
+    dados_filtrados = utils.filtra_dados_por_valores_procurados(dados, coluna_principio_ativo, principios_ativos_cloroquina)
+    soma_vendas = soma_vendas_por_atributo(dados_filtrados, coluna_estados)
+    dados_estaduais = mapeia_dados_estaduais(soma_vendas, coluna_estados)
+    
+    dados_estaduais.plot(column="vendas", cmap=cmap, vmax=vmax, vmin=0, ax=ax, legend=True)
+
+    if show_figure == True:
+        plt.show()
+
+    return dados_estaduais
+
+
 def visualizacao_sillas():
     pass
 
 
 if __name__ == "__main__":
-    # dados = utils.concat_data_by_dates("2014/01", "2014/01")
+    # dados = utils.concat_data_by_dates("2021/01", "2021/01")
 
-    # dados = filtra_dados_por_valores_procurados(dados, "CID10", "A00")
+    # print(gera_visualizacao_cloroquina(dados, show_figure=True))
 
-    # dados = soma_vendas_por_atributo(dados, "UF_VENDA")
 
-    # dados = mapeia_dados_estaduais(dados, "UF_VENDA")
-
-    # print(dados)
-
-    # print(data["UF_VENDA"].value_counts())
+    pass
