666c880d1112b1b453d4f8b882013dc5327b4a07
LEONARDO ALEXANDRE DA SILVA FERREIRA <B51094@fgv.edu.br>
2023-10-15 11:01:29 -0300

    Realizei mudanças nas funções

--- functions/Leonardo_vis.py
+++ functions/Leonardo_vis.py
@@ -14,16 +14,34 @@ def dataframe_de_zolpidem(dataframe_selecionado: pd.DataFrame) -> pd.DataFrame:
         Dataframe referente ao remédio Zolpidem.
 
     Returns
-    -------
+    ----------
     df_venda_por_ano: pd.Dataframe
         Dataframe das quantidades vendidas por ano. 
+
+    Examples
+    ----------
+    >>> dataframe_de_zolpidem(utils.concat_data_by_dates("2014/01", "2020/12", filtered_columns = ["ANO_VENDA", "PRINCIPIO_ATIVO"]))
+
+    >>> dataframe_de_zolpidem(2023)
+    O dataframe é inválido. 
+
+    >>> dataframe_de_zolpidem(pd.Dataframe())  
+    O dataframe está vazio. 
 
     """
 
     # Gerando o dataframe que contém apenas o Zolpidem
+    
+    try:
+        df_hemitartarato_de_zolpidem = (dataframe_selecionado[dataframe_selecionado["PRINCIPIO_ATIVO"] == "HEMITARTARATO DE ZOLPIDEM"]).reset_index(drop=True)
+    except AttributeError:
+        return "O dataframe não contém a coluna PRINCIPIO_ATIVO"
 
-    df_hemitartarato_de_zolpidem = (dataframe_selecionado[dataframe_selecionado["PRINCIPIO_ATIVO"] == "HEMITARTARATO DE ZOLPIDEM"]).reset_index(drop=True)
-    df_zolpidem = (dataframe_selecionado[dataframe_selecionado["PRINCIPIO_ATIVO"] == "ZOLPIDEM"]).reset_index(drop=True)
+    try:
+        df_zolpidem = (dataframe_selecionado[dataframe_selecionado["PRINCIPIO_ATIVO"] == "ZOLPIDEM"]).reset_index(drop=True)
+    except AttributeError:
+        return "O dataframe não contém a coluna PRINCIPIO_ATIVO"
+
     df = pd.concat([df_hemitartarato_de_zolpidem, df_zolpidem]).reset_index()
 
     # Contagem por ano
@@ -36,22 +54,30 @@ def dataframe_de_zolpidem(dataframe_selecionado: pd.DataFrame) -> pd.DataFrame:
 
 print(dataframe_de_zolpidem(utils.concat_data_by_dates("2014/01", "2020/12", filtered_columns = ["ANO_VENDA", "PRINCIPIO_ATIVO"])))
 
-"""
-if __name__ == "__main__":
+def visualizacao_leonardo(dataframe_de_vendas_anuais: pd.DataFrame) -> pd.DataFrame:
+    """
+    A função recebe um dataframe que possui a quantidade de vendas do remédio Zolpidem por ano e retorna
+    um gráfico de linhas baseado no dataframe.
 
-    # Dataframes de teste
+    Parameters
+    ----------
+    dataframe_de_vendas_anuais: pd.DataFrame
+        Dataframe que contém o total de vendas anuais do remédio Hemitartarato de Zolpidem
 
-    dataframe_especificado = utils.concat_data_by_dates("2014/01", "2020/12", filtered_columns = ["ANO_VENDA", "PRINCIPIO_ATIVO"])
-    dataframe_vazio = pd.DataFrame()
+    Returns
+    -------
+    "Visualização finalizada": str
+        Mensagem de conclusão da visualização.
 
-    doctest.testmod()
-"""
+    """
 
-def visualizacao_leonardo(dataframe_reduzido: pd.DataFrame) -> pd.DataFrame:
+    try:
+        df_venda_por_ano = dataframe_de_zolpidem(dataframe_de_vendas_anuais)
+    except ValueError:
+        return "Não foi fornecido um dataframe válido."    
+    
 
-    df_venda_por_ano = dataframe_de_zolpidem(dataframe_reduzido)
-
-    # Customização do gráfico
+    # Construção do gráfico
 
     plt.scatter(df_venda_por_ano["ANO_VENDA"], df_venda_por_ano["REMÉDIO_VENDIDO"], marker="*", c = "Black")
     plt.plot(df_venda_por_ano["ANO_VENDA"], df_venda_por_ano["REMÉDIO_VENDIDO"], c = "Gray")
@@ -60,6 +86,13 @@ def visualizacao_leonardo(dataframe_reduzido: pd.DataFrame) -> pd.DataFrame:
     plt.ylabel("Vendas", fontweight = "bold")
     plt.gca().set_facecolor("Beige")
 
+    plt.savefig("functions\Leonardo_imagem\\zolpidem.png")
+
     plt.show()
 
-print(visualizacao_leonardo(utils.concat_data_by_dates("2014/01", "2020/12", filtered_columns = ["ANO_VENDA", "PRINCIPIO_ATIVO"])))    
+    return "Visualização finalizada"
+
+# print(visualizacao_leonardo(utils.concat_data_by_dates("2014/01", "2020/12", filtered_columns = ["ANO_VENDA", "PRINCIPIO_ATIVO"])))  
+
+if __name__ == "__main__":
+    doctest.testmod(verbose = True)
