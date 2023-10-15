4147e84a0e40d09851fa68f9255c34bf0cebc8ed
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-10-15 09:23:14 -0300

    Ajuste na função luciano_vis e doctests

--- functions/luciano_vis.py
+++ functions/luciano_vis.py
@@ -2,16 +2,29 @@ import pandas as pd
 import utils
 import matplotlib.pyplot as plt
 import doctest
+from typing import Literal, Iterable, Optional
+from numpy import datetime64
 
 def filtra_e_contatena(coluna_do_valor: str, valores_procurados: list | str) -> pd.DataFrame:
     '''Usa as funções do módulo utils para concatenar todas as datas disponíveis e pegar apenas as linhas com os valores procurados
 
     Parâmetros:
-        coluna_do_valor (str): nome da coluna em que o valor procurado deve estar
-        valores_procurados (list | str): valor, caso string, ou valores, caso lista, que serão procurados na coluna
+    ------------
+    coluna_do_valor:
+        Tipo: str 
+        Descrição: 
+            nome da coluna em que o valor procurado deve estar
+
+    valores_procurados: 
+        Tipo: list | str
+        Descrição: 
+            valor, caso string, ou valores, caso lista, que serão procurados na coluna
     
     Retorna:
-        dataframe (pandas.DataFrame): dataframe pandas com os dados filtrados
+    ---------
+    dataframe:
+        Tipo: pandas.DataFrame
+        Descrição: dataframe pandas com os dados filtrados
     
     '''
     dataframe = utils.concat_data_by_dates(f"2014/01", f"2014/12")
@@ -29,10 +42,17 @@ def cria_datetime(dataframe: pd.DataFrame) -> pd.Series:
     Os dias no datetime serão todos o primeiro dia de cada mês.
 
     Parâmetros:
-        dataframe (pd.Dataframe): o dataframe do Pandas com as colunas "ANO_VENDA" e "MES_VENDA".
+    -------------
+    dataframe: 
+        Tipo: pandas.DataFrame
+        Descrição:
+            o dataframe do Pandas com as colunas "ANO_VENDA" e "MES_VENDA".
 
     Retorna:
-        datetimes (pd.Series): série do Pandas com as datas em formato datetime.
+    ---------
+    datetimes: 
+        Tipo: pandas.Series
+        Descrição: série do Pandas com as datas em formato datetime.
     
     '''
     datetimes = {"Year": dataframe["ANO_VENDA"],
@@ -43,26 +63,32 @@ def cria_datetime(dataframe: pd.DataFrame) -> pd.Series:
 
 def regiao_estado(sigla_estado: str) -> str:
     """
-    Retorna a região do estado brasileiro: Norte, Nordeste, Centro-Oeste, Sudeste, Sul
+    Retorna a região do estado brasileiro: Norte, Nordeste, Centro-Oeste, Sudeste ou Sul.
     
     Parâmetros
     -------------
     sigla_estado:
         Tipo: str
-        Descrição: Sigla do estado brasileiro
+        Descrição: 
+            Sigla do estado brasileiro
         Exemplo: "MG"
     
     Retorna:
     --------
     regiao:
-        Tipo: str
-        Descrição: Região em que o estado brasileiro está.
+        Tipo: str | None
+        Descrição: 
+            Região em que o estado brasileiro está. 
+            Se a sigla inserida for inexistente, retorna None.
         Exemplo: Sudeste
     
     Exemplos
     ----------
     >>> regiao_estado("RR")
     'Norte'
+
+    >>> regiao_estado("BH")
+    
     """
 
     estados = {
@@ -94,41 +120,42 @@ def regiao_estado(sigla_estado: str) -> str:
         "SC": "Sul",
         "RS": "Sul"
     }
-    regiao = estados[sigla_estado]
+    try:
+        regiao = estados[sigla_estado]
+    except KeyError:
+        return None
+    
     return regiao
 
-def grafico_linhas(datas, valores: tuple, labels: tuple[str] | str, titulo: str = None, transparencias: tuple[float] | float = 1.0):
+def grafico_linhas(datas: Iterable[datetime64], valores: tuple[Iterable] | Iterable, labels: tuple[str] | str, titulo: str | None = None, transparencias: tuple[float] | float = 1.0):
     """Prepara um gráfico de linhas com os dados fornecidos usando o matplotlib.pyplot
     
     Parâmetros
     -------------
     datas:
         Tipo: Iterable[datetime]
-        Descrição: Datas que serão usadas no eixo X.
+        Descrição: 
+            Datas que serão usadas no eixo X.
     
     valores:
-        Tipo: 
-            tuple[Iterable] | Iterable
+        Tipo: tuple[Iterable] | Iterable
         Descrição: 
             Valores que serão plotados em relação ao eixo Y. 
             Se há mais de uma linha, o parâmetro deve receber uma tupla com os dados de cada linha.
     
     labels:
-        Tipo: 
-            tuple[str] | str
+        Tipo: tuple[str] | str
         Descrição:
             Legendas das linhas a serem plotadas. 
             Se há mais de uma linha, esse parâmetro deve receber uma tupla.
     
     titulo:
-        Tipo:
-            str
+        Tipo: str | None
         Descrição:
             Título do gráfico.
     
     transparencias:
-        Tipo:
-            tuple[float] | float
+        Tipo: tuple[float] | float
         Descrição:
             Transparências das linhas a serem geradas. 
             Se há mais de uma linha, esse parâmetro deve receber uma tupla.
@@ -153,7 +180,6 @@ def grafico_linhas(datas, valores: tuple, labels: tuple[str] | str, titulo: str 
 
     plt.legend(loc='best')
     plt.suptitle(titulo)
-    plt.show()
 
 def contagem_elementos(dataframe: pd.DataFrame, elemento_contado: str, nome_da_serie: str) -> pd.Series:
     """
@@ -163,21 +189,25 @@ def contagem_elementos(dataframe: pd.DataFrame, elemento_contado: str, nome_da_s
     -------------
     dataframe:
         Tipo: pandas.DataFrame
-        Descrição: DataFrame a ser modificado
+        Descrição: 
+            DataFrame a ser modificado
 
     elemento_contado:
         Tipo: str
-        Descrição: Nome da coluna em que o value_counts vai ser feito
+        Descrição: 
+            Nome da coluna em que o value_counts vai ser feito
     
     nome_da_serie:
         Tipo: str
-        Descrição: nome da série a ser retornada
+        Descrição: 
+            nome da série a ser retornada
     
     Retorna
     --------
     contagem:
         Tipo: pandas.Series
-        Descrição: série do pandas com as informações feitas
+        Descrição: 
+            série do pandas com as informações feitas
     
     Exemplos
     --------
@@ -196,8 +226,37 @@ def contagem_elementos(dataframe: pd.DataFrame, elemento_contado: str, nome_da_s
 
     return contagem
 
-def luciano_vis():
-    """Gera o gráfico escolhido por Luciano"""
+def luciano_vis(acao: Literal["show", "save", None], path: str|None = None):
+    """
+    Gera o gráfico escolhido por Luciano. Vai ser exibido ou salvo.
+
+    Parâmetros:
+    ------------
+    acao:
+        Tipo: str | None
+        Descrição: 
+            Ação a ser executada depois de formar o gráfico. Pode ser:
+            "show": irá mostrar o gráfico e fechar a figura.
+            "save": irá salvar o gráfico e fechar a figura.
+            None: formará o gráfico, mas não o fechará.
+    
+    path:
+        Tipo: str | None
+        Descrição: 
+            path em que a figura será salva. É necessário caso o parâmetro acao seja "save".
+    
+    Exemplo:
+    -----------
+    >>> luciano_vis(acao = "salvar")
+    Traceback (most recent call last):
+    ...
+    ValueError: Ação 'salvar' inválida. 
+    Valores válidos: 'show', 'save', None
+
+    """
+    if acao not in ["show", "save", None]:
+        raise ValueError(f"Ação '{acao}' inválida. \nValores válidos: 'show', 'save', None")
+    
     principios_de_antidepressivos = ["SELEGILINA","SERTRALINA", "AMITRIPTILINA", "CITALOPRAM",
                                      "CLOMIPRAMINA", "IPRONIAZIDA", "MOCLOBEMIDA", "IMIPRAMINA",
                                      "TRIMIPRAMINA", "NORTRIPTILINA", "PROTRIPTILINA", "DOXEPINA",
@@ -209,7 +268,6 @@ def luciano_vis():
     
     try:
         dataframe_antidepressivos = pd.read_csv("dados/Manipulados_ANTIDEPRESSIVOS.csv")
-        print("CSV lido!")
     except:
         dataframe_antidepressivos = filtra_e_contatena("PRINCIPIO_ATIVO", principios_de_antidepressivos)
         dataframe_antidepressivos.to_csv("dados/Manipulados_ANTIDEPRESSIVOS.csv", index=False)
@@ -238,7 +296,16 @@ def luciano_vis():
                                     contagem["SUDESTE"],
                                     contagem["SUL"]), 
                                     tuple(contagem.columns), titulo = "ANTIDEPRESSIVOS POR REGIÃO")
+    if acao == "show":
+        plt.show()
+        plt.close()
+    elif acao == "save":
+        plt.save(path)
+        plt.close()
+
+
 
 if __name__ == "__main__":
+    luciano_vis("show")
     doctest.testmod(verbose=True)
     
