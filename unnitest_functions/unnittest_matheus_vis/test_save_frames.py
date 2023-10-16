417b64df695b05ebc961efcd5aa9e35a528450e1
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-10-16 01:11:46 -0300

    consertei o import relativo de um unnittest
    agora ele está rodando sem problemas

--- unnitest_functions/unnittest_matheus_vis/test_save_frames.py
+++ unnitest_functions/unnittest_matheus_vis/test_save_frames.py
@@ -7,13 +7,14 @@ mes analizado e o caminho para a pasta onde a imagem deverá ser salva.
 import sys, os
 
 esse_caminho = os.path.dirname(os.path.abspath(__file__))
-caminho_functions = os.path.join(esse_caminho, "..", "functions")
+caminho_functions = os.path.join(esse_caminho, "..", "..", "functions")
 sys.path.append(caminho_functions)
+
 
 import matplotlib.pyplot as plt
 import os
 import unittest
-import pandas as pd  
+import pandas as pd
 from utils import set_anabolizantes
 from matheus_vis import save_frames, gerador_de_frames
 
